db9d8a0ada2a033a09b8af40eca9b492d8b4e1f5
xocomil <xocomil@gmail.com>
2022-06-16 18:22:28 -0600

    added the `grid-analyzers` library

--- apps/angular-sudoku/src/app/app.module.ts
+++ apps/angular-sudoku/src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { GridComponentModule } from '@sud/components';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, GridComponentModule],
   providers: [],
   bootstrap: [AppComponent],
 })
