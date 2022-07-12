e1da18b7d4f9bc0a62297e29ce3cc867e85daeaf
xocomil <xocomil@gmail.com>
2022-07-11 18:49:08 -0600

    handling the input for cells

--- libs/components/src/lib/cell/cell.component.ts
+++ libs/components/src/lib/cell/cell.component.ts
@@ -4,6 +4,7 @@ import {
   Component,
   EventEmitter,
   HostBinding,
+  HostListener,
   Input,
   NgModule,
   Output,
@@ -19,6 +20,8 @@ import produce from 'immer';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CellComponent {
+  readonly #allowedValues = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
   @Input() cellState: CellState = createCellState({
     row: -1,
     column: -1,
@@ -41,9 +44,29 @@ export class CellComponent {
   @Output() cellBlurReceived = new EventEmitter<void>();
   @Output() cellValueChanged = new EventEmitter<CellState>();
 
-  inputValueChanged(newValue: number): void {
+  #getNumericValue(newValue: string): number | undefined {
+    const numericValue = parseInt(newValue, 10);
+
+    if (isNaN(numericValue)) {
+      return undefined;
+    }
+
+    return numericValue;
+  }
+
+  @HostListener('keypress', ['$event'])
+  protected handleKeyEvent(event: KeyboardEvent) {
+    console.log('event', event);
+    event.preventDefault();
+
+    if (this.#allowedValues.includes(event.key)) {
+      this.#inputValueChanged(event.key);
+    }
+  }
+
+  #inputValueChanged(newValue: string): void {
     this.cellState = produce(this.cellState, (draft) => {
-      draft.value = newValue;
+      draft.value = this.#getNumericValue(newValue);
     });
 
     this.cellValueChanged.emit(this.cellState);
