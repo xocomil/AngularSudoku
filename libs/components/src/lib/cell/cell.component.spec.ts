4d1b6b2ca57aa87cd49967c08076195f57016ab5
xocomil <xocomil@gmail.com>
2022-08-08 20:02:11 -0600

    add some tests for the `CellComponent`

--- libs/components/src/lib/cell/cell.component.spec.ts
+++ libs/components/src/lib/cell/cell.component.spec.ts
@@ -1,5 +1,6 @@
 import { FormsModule } from '@angular/forms';
-import { createComponentFactory } from '@ngneat/spectator';
+import { faker } from '@faker-js/faker/locale/en';
+import { createComponentFactory } from '@ngneat/spectator/jest';
 import { CellComponent } from './cell.component';
 
 describe('CellComponent', () => {
@@ -8,9 +9,62 @@ describe('CellComponent', () => {
     imports: [FormsModule],
   });
 
+  const randomCellPosition = (): number =>
+    faker.datatype.number({ min: 0, max: 8 });
+  const randomNextToFocus = (): readonly [number, number] =>
+    [randomCellPosition(), randomCellPosition()] as const;
+
   it('should create', () => {
     const spectator = createComponent();
 
     expect(spectator).toBeTruthy();
   });
+
+  describe('nextToFocus', () => {
+    describe('undefined', () => {
+      it('should set the value to undefined initially', () => {
+        const spectator = createComponent({
+          props: { nextToFocus: undefined },
+        });
+
+        expect(spectator.component.nextToFocus).toBeUndefined();
+      });
+
+      it('should change the value to undefined', () => {
+        const spectator = createComponent({
+          props: { nextToFocus: randomNextToFocus() },
+        });
+
+        spectator.component.nextToFocus = undefined;
+
+        expect(spectator.component.nextToFocus).toBeUndefined();
+      });
+    });
+
+    describe('a valid value', () => {
+      it('should set the value initially', () => {
+        const testValue = randomNextToFocus();
+
+        const spectator = createComponent({
+          props: { nextToFocus: testValue },
+        });
+
+        expect(spectator.component.nextToFocus).toEqual(testValue);
+      });
+
+      it('should change the value to a valid value', () => {
+        const testValue = randomNextToFocus();
+
+        const spectator = createComponent({
+          props: { nextToFocus: undefined },
+        });
+
+        spectator.component.nextToFocus = testValue;
+
+        expect(spectator.component.nextToFocus).toEqual(testValue);
+      });
+    });
+  });
+
+  describe('ngOninit', () => {});
 });
