b70165c6032a69b29e473b1df085a08ec32b6748
Jason Warner <xocomil@gmail.com>
2024-06-10 19:14:14 -0600

    changed to zoneless Angular

---
+++ apps/angular-sudoku/src/app/app.config.ts
@@ -0,0 +1,8 @@
+import {
+  ApplicationConfig,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideExperimentalZonelessChangeDetection()],
+};
