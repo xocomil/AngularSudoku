e81aea6a876296973d071de3a0a4985c7f06051a
Jason Warner <xocomil@gmail.com>
2024-09-15 11:51:54 -0600

    added `withManageSelected()` feature

--- libs/components/src/lib/grid/store/grid.store.ts
+++ libs/components/src/lib/grid/store/grid.store.ts
@@ -1,8 +1,10 @@
 import { signalStore, withState } from '@ngrx/signals';
+import { withManageSelected } from './grid-manage-seleccted.feature';
 import { withGridComputed } from './grid.computed.feature';
 import { initialState } from './grid.state';
 
 export const GridStore = signalStore(
   withState(initialState()),
   withGridComputed(),
+  withManageSelected(),
 );
