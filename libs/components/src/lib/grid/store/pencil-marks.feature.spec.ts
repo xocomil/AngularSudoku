f15df160c993b0f71d20f241652f1af57473095b
Jason Warner <xocomil@gmail.com>
2025-01-05 11:35:08 -0700

    adding tests for pencil marks

---
+++ libs/components/src/lib/grid/store/pencil-marks.feature.spec.ts
@@ -0,0 +1,19 @@
+describe('withPencilMarks Feature', () => {
+  it('should set row pencil marks', () => {
+    // Arrange
+    // Act
+    // Assert
+  });
+
+  it('should set updated column pencil marks', () => {
+    // Arrange
+    // Act
+    // Assert
+  });
+
+  it('should set updated region pencil marks', () => {
+    // Arrange
+    // Act
+    // Assert
+  });
+});
