058796bf9ee423a1d12584c53b200aa3a3a2814d
Jason Warner <xocomil@gmail.com>
2025-03-30 09:48:27 -0600

    remove unused variable

--- libs/components/src/lib/grid/store/grid-errors.feature.ts
+++ libs/components/src/lib/grid/store/grid-errors.feature.ts
@@ -40,7 +40,6 @@ export function withGridErrors<_>() {
       },
       _updateCellValid(errors: boolean[][]) {
         const grid = state.grid();
-        let hasError = false;
 
         for (let row = 0; row < grid.length; row++) {
           for (let col = 0; col < grid[row].length; col++) {
